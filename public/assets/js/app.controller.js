5b6ede2074708b7e2d4859a2c5c3bc412c1ef8c2
Nullises <texanico@gmail.com>
2017-12-19 16:25:04 -0400

    Module & Controller added

---
+++ public/assets/js/app.controller.js
@@ -0,0 +1,13 @@
+(function(){
+  'use strict';
+
+  angular
+        .module('WooCommerceSensacional')
+        .controller('WooCommerceSensacionalController', WooCommerceSensacionalController);
+
+    WooCommerceSensacionalController.$inject = ['$scope', '$timeout'];
+
+    function WooCommerceSensacionalController ($scope, $timeout) {
+      
+    }
+})();
---
+++ public/assets/js/app.module.js
@@ -0,0 +1,9 @@
+(function(){
+  'use strict';
+
+  angular.module('WooCommerceSensacional', [
+    'ngMaterial',
+    'ngAria',
+    'ui.bootstrap'
+  ]);
+})();
